0f8ac623c164866a61de6961940cca3bc9ff23f9
Admin <Admin@DESKTOP-UGP0C2G>
2024-11-30 09:43:42 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿// 4-х значное  
+//Console.Write("Введите четырехзначное число: ");
+//     string input = Console.ReadLine();
+//     if (input.Length != 4)
+//     {
+//         Console.WriteLine("Ошибка! Вы ввели не четырехзначное число.");
+//         return;
+//     }
+
+//     int sum = 0;
+//     foreach (char digit in input)
+//     {
+//         int number = digit - '0';
+//         sum += number;
+//     }
+//     Console.WriteLine($"Сумма цифр введенного числа: {sum}");
+// 5 значное 
+Console.Write("Введите пятизначное число: ");
+string input = Console.ReadLine();
+if (input.Length != 5)
+{
+    Console.WriteLine("Ошибка! Вы ввели не пятизначное число.");
+    return;
+}
+
+int sum = 0;
+foreach (char digit in input)
+{
+    int number = digit - '0';
+    sum += number;
+}
+Console.WriteLine($"Сумма цифр введенного числа: {sum}");
